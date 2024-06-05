5b40378a4a96c3697ef7ddfdcaf8de504769c7ab
digwijay <digwijay2@gmail.com>
2024-06-05 12:47:58 +0530

    Created React app

---
+++ client/React-App/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ //Test your Modules here
+
+  return (
+    <>
+    <h3>Test inside</h3>
+      
+    </>
+  )
+}
+
+export default App
