58f304dfb51e5b94cdfa96952991dd6550fffdce
digwijay <digwijay2@gmail.com>
2024-06-05 13:10:21 +0530

    added Pages and Styles

--- client/React-App/src/App.jsx
+++ client/React-App/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Seller from './pages/Seller'
 import './App.css'
 
 function App() {
@@ -9,6 +7,7 @@ function App() {
   return (
     <>
     <h3>Test inside</h3>
+    <Seller/>
       
     </>
   )
---
+++ client/React-App/src/pages/Seller.jsx
@@ -0,0 +1,12 @@
+import '../styles/Seller.css'
+
+function Seller(){
+    return(
+        <>
+        <p className="Text-seller">This Is Seller Page</p>
+        
+        </>
+    )
+}
+
+export default Seller
