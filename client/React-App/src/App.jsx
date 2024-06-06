52ba00a570c9c764058e6725aecf5d64ec6aed16
digwijay <digwijay2@gmail.com>
2024-06-06 19:41:23 +0530

    3rd Update on navbar

--- client/React-App/src/App.jsx
+++ client/React-App/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import ManageProp from './pages/ManageProp.jsx'
+import PropertySts from './pages/PropertySts.jsx'
+import Property from './pages/Property.jsx'
+import LoginSignup from './pages/LoginSignup.jsx'
 
-function App() {
-  //Test your Modules here
-
+const App = () => {
   return (
-    <div className='container' >
-      <Navbar />
-
+    <div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/> } />
+      <Route path='/ManageProperty' element={<ManageProp/> } />
+      <Route path='/propertyStatus' element={<PropertySts/> } />
+      <Route path='/property' element={<Property/>}/>
+      <Route path=':propertId' element={<Property/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
--- client/React-App/src/components/Navbar.jsx
+++ client/React-App/src/components/Navbar.jsx
@@ -1,35 +1,77 @@
-import React from 'react'
-import '../styles/Navbar.css'
-import logo from '../assets/logo-react.png'
-import search from '../assets/search-icon.png'
-import profile from '../assets/profile-pic.jpg'
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+import logo from "../assets/logo-react.png";
+import search from "../assets/search-icon.png";
+import profile from "../assets/profile-pic.jpg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-
-        <div className='navbar'>
-            <img src={logo} alt="" className='logo' />
-            <ul>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Request</a> </li>
-                <li> <a href="">Add New</a> </li>
-                <li> <a href="">About</a> </li>
-            </ul>
-           {/* <div className='search-box'>
+  const [menu, setMenu] = useState("Home");
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setMenu("Home");
+            }}
+            className={menu === "Home" ? "active" : "list-item"}
+          >
+            Home
+          </li>
+        </Link>
+        <Link to="/ManageProperty" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setMenu("ManageProperty");
+            }}
+            className={menu === "ManageProperty" ? "active" : "list-item"}
+          >
+            Manage Property
+          </li>
+        </Link>
+        <Link to="/PropertyStatus" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setMenu("PropertyStatus");
+            }}
+            className={menu === "PropertyStatus" ? "active" : "list-item"}
+          >
+            Property Status
+          </li>
+        </Link>
+        <Link to="/#" style={{ textDecoration: "none" }}>
+          <li
+            onClick={() => {
+              setMenu("About");
+            }}
+            className={menu === "About" ? "active" : "list-item"}
+          >
+            About
+          </li>
+        </Link>
+      </ul>
+      {/* <div className='search-box'>
                 <input type="text" placeholder="search" />
                 <img src={search} alt="" />
-            </div>
-           <div className='Profile'>
-                <img src={profile} alt="profile" className='profile-pic' />
-                <p className='seller-name'>Stitch</p>
-    </div> */}
-    <div className='Entry'> 
-        <button >Login</button>
-        <button >Signup</button>
-    </div>
-
+            </div>*/}
+      <div className="Profile-btn">
+        <div className="Entry">
+          <button>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              Logout
+            </Link>
+          </button>
         </div>
-    )
-}
 
-export default Navbar
+        <div className="Profile">
+          <img src={profile} alt="profile" className="profile-pic" />
+          <p className="seller-name">Stitch</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
