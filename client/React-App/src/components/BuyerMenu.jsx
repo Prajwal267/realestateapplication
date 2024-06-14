165acbeddbddc14e9f2c42ff4bacdbe5835f3663
karthi9061 <karthik268743@gmail.com>
2024-06-14 12:40:40 +0530

    styles

--- client/React-App/src/components/BuyerMenu.jsx
+++ client/React-App/src/components/BuyerMenu.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/BuyerMenu.css';
+
 const BuyerMenu = () => {
     const location = useLocation();
 
     return (
         <div className="property-search">
-            <div className="flex space-x-4 mb-3" role="group">
-                <Link 
-                    to="/Buy" 
-                    className={`btn text-secondary ${location.pathname === '/Buy' ? 'active' : ''} py-2 px-4 rounded`}
+            <div className="flex">
+                <Link
+                    to="/Buy"
+                    className={`btn ${location.pathname === '/Buy' ? 'active' : ''}`}
                     aria-current={location.pathname === '/Buy' ? 'page' : undefined}
                 >
                     Buy
                 </Link>
-                <Link 
-                    to="/Rent" 
-                    className={`btn text-secondary ${location.pathname === '/Rent' ? 'active' : ''}  py-2 px-4 rounded`}
+                <Link
+                    to="/Rent"
+                    className={`btn ${location.pathname === '/Rent' ? 'active' : ''}`}
                     aria-current={location.pathname === '/Rent' ? 'page' : undefined}
                 >
                     Rent
                 </Link>
-                <Link 
-                    to="/CommercialPage" 
-                    className={`btn text-secondary ${location.pathname === '/CommercialPage' ? 'active' : ''}  py-2 px-4 rounded`}
+                <Link
+                    to="/CommercialPage"
+                    className={`btn ${location.pathname === '/CommercialPage' ? 'active' : ''}`}
                     aria-current={location.pathname === '/CommercialPage' ? 'page' : undefined}
                 >
-                    Comercial
+                    Commercial
                 </Link>
             </div>
         </div>
