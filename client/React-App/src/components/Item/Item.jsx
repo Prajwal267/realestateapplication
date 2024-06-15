6c89d64af6473eda436768fb018bee5a52b111f9
digwijay <digwijay2@gmail.com>
2024-06-15 13:48:01 +0530

    update item

--- client/React-App/src/components/Item/Item.jsx
+++ client/React-App/src/components/Item/Item.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import "./Item.css";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Item = (props) => {
   return (
 
-    <Card className="clm">
+
+<div className="card card-custom">
+<img src={props.image} className="card-img-top" alt="Property" />
+{/* <div className="premium-badge">Sale</div> */}
+<div className="card-body">
+    <h5 className="card-title">{props.name}</h5>
+    <p className="card-text">{props.item_price}</p>
+    <p className="card-text">{props.description}</p> {/* Display BHK type */}
+    <button type="button" className="btn btn-custom btn-sm">View Property</button>
+</div>
+</div>
+
+);
+};
+
+export default Item;
+
+
+   {/* <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <p>{props.description}</p>
+      <div className="item-price">{props.item_price}</div>
+    </div>*/}
+
+
+    {/*    <Card className="clm">
     <Card.Img variant="top" src={props.image} className="img-fluid"/>
     <Card.Body>
       <Card.Title>{props.name}</Card.Title>
@@ -24,18 +48,4 @@ const Item = (props) => {
 
     </Card.Body>
   </Card>
-
-
-
-);
-};
-
-export default Item;
-
-
-   {/* <div className="item">
-      <img src={props.image} alt="" />
-      <p>{props.name}</p>
-      <p>{props.description}</p>
-      <div className="item-price">{props.item_price}</div>
-    </div>*/}
+*/}
